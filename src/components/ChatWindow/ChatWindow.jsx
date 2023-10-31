d5e1755d41b6eff25493232570ab1905129d87c9
TriTran <tranhuutrict@gmail.com>
2023-10-31 11:35:55 +0700

    development: create custom hook for saving and  managing chat messages

--- src/components/ChatWindow/ChatWindow.jsx
+++ src/components/ChatWindow/ChatWindow.jsx
@@ -1,6 +1,14 @@
 import styles from "./ChatWindow.module.scss";
+import { useConversation } from "../../hooks";
+import { useEffect } from "react";
 
 const ChatWindow = () => {
+  const [conversation] = useConversation();
+
+  useEffect(() => {
+    console.log("conversation: ", conversation);
+  }, [conversation]);
+
   return (
     <div className={styles.wrapper}>
       <p>chat page</p>
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,1 +1,3 @@
-export {};
+import useConversation from "./useConversation";
+
+export { useConversation };
