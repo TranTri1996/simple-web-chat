dc4afed0f3f894176230d1abca56efe3ef312997
TriTran <tranhuutrict@gmail.com>
2023-10-31 16:07:36 +0700

    development: add unit test for custo hook

---
+++ src/hooks/useMoreMessages/useMoreMessages.test.js
@@ -0,0 +1,15 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import useMoreMessages from "./useMoreMessages";
+
+describe("useMoreMessages", () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  it("should initialize with an empty messages array", () => {
+    const { result } = renderHook(() => useMoreMessages(5));
+
+    const { messages } = result.current;
+    expect(messages).toEqual([]);
+  });
+});
