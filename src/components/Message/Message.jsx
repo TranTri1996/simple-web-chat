d659a0ac26c04bc659d4f36c60ccc0d48d9c5fd1
TriTran <tranhuutrict@gmail.com>
2023-10-31 12:15:06 +0700

    development: create Message component for displaying each message on the conversion

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,11 +1,31 @@
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./Message.module.scss";
 
 const Message = ({ userName, message, onLeftSide }) => {
   return (
-    <div className={styles.wrapper}>
-      <p>{`${userName}: ${message}`}</p>
+    <div className={onLeftSide ? styles.wrapperLeft : styles.wrapperRight}>
+      <p className={styles.userName}>{userName}</p>
+      <div className={styles.content}>
+        <div className={classNames(styles.message)}>
+          <span>{message}</span>
+          <div className={styles.arrow} />
+        </div>
+        <div className={styles.avatar}>
+          <FontAwesomeIcon icon={faUser} className={styles.icon} />
+        </div>
+      </div>
     </div>
   );
+};
+
+Message.propTypes = {
+  userName: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  onLeftSide: PropTypes.bool.isRequired,
 };
 
 export default Message;
